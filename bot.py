10ff21691f651dea4526b2408fea3da48a2272d3
Denis <mistrny@gmail.com>
2022-12-10 16:25:52 +0000

    add multiple photos per message

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ import yaml
 from loguru import logger
 
 from telebot import TeleBot, types  # для указание типов
-
+from telebot.formatting import escape_markdown
 with open("config.yaml") as f:
     cfg = yaml.safe_load(f)
     bot = TeleBot(cfg["token"])
     CHANEL_ID = str(cfg["chanel_id"])
 
-user_message = defaultdict(dict)
+user_message = defaultdict(lambda: defaultdict(int))
 
 
 @bot.message_handler(commands=["start"])
@@ -26,15 +26,19 @@ def start_message(message):
 
 @bot.message_handler(content_types=["photo"])
 def handle_docs_photo(message):
-    global user_message
     user_id = message.from_user.id
-    if len(user_message[user_id]) < 2 or (time() - user_message[user_id]["last_time"] > 60):
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton(text="Открыто", callback_data="deanon"))
-        markup.add(types.InlineKeyboardButton(text="Анонимно", callback_data="anon"))
-        markup.add(types.InlineKeyboardButton(text="Я передумал(а)", callback_data="reset"))
-        bot.send_message(message.chat.id, text="Как опубликовать спящего?", reply_markup=markup)
-        user_message[user_id]["message"] = message
+    if len(user_message[user_id]) < 2 or (time() - user_message[user_id]["last_time"] > 1):
+        if user_message[user_id]["handler_trigger"] == 0:
+            user_message[user_id]["handler_trigger"] = 1
+            user_message[user_id]["message"] = [message]
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(text="Открыто", callback_data="deanon"))
+            markup.add(types.InlineKeyboardButton(text="Анонимно", callback_data="anon"))
+            markup.add(types.InlineKeyboardButton(text="Я передумал(а)", callback_data="reset"))
+            bot.send_message(message.chat.id, text="Как опубликовать спящего?", reply_markup=markup)
+        else:
+            user_message[user_id]["message"].append(message)
+
     else:
         text_2_send = (
             "Фото можно отправлять раз в минуту,"
@@ -46,24 +50,52 @@ def handle_docs_photo(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    global user_message, CHANEL_ID
     if call.data == "reset":
         bot.delete_message(call.message.chat.id, call.message.message_id)
     else:
+        signiture = None
+        caption = user_message[call.from_user.id]["message"][0].caption
         if call.data == "deanon":
-            bot.forward_message(CHANEL_ID, call.from_user.id, user_message[call.from_user.id]["message"].message_id)
-            logger.info(f"Бот открыто отправил  картинку от @{call.from_user.username}")
-        elif call.data == "anon":
+            signiture = (
+                f"\nПрислал [{call.from_user.first_name} {call.from_user.last_name}](tg://user?id={call.from_user.id})"
+            )
+
+        result_caption = ""
+        
+        if caption is not None:
+            result_caption += escape_markdown(caption)
+        if signiture is not None:
+            result_caption += signiture
+
+        if len(user_message[call.from_user.id]["message"]) > 1:
+            media = []
+            for idx, msg in enumerate(user_message[call.from_user.id]["message"]):
+                result_caption = result_caption if idx == 0 else None
+                media.append(
+                    types.InputMediaPhoto(
+                        media=msg.photo[-1].file_id,
+                        caption=result_caption,
+                        parse_mode="MarkdownV2",
+                    )
+                )
+            bot.send_media_group(CHANEL_ID, media=media)
+        else:
             bot.send_photo(
                 CHANEL_ID,
-                photo=user_message[call.from_user.id]["message"].photo[-1].file_id,
-                caption=user_message[call.from_user.id]["message"].caption,
+                photo=user_message[call.from_user.id]["message"][0].photo[-1].file_id,
+                caption=result_caption,
+                parse_mode="MarkdownV2",
             )
-            logger.info(f"Бот анонимно отправил  картинку от @{call.from_user.username}")
+        if call.data == "deanon":
+            logger.info(f"Бот открыто отправил картинку от @{call.from_user.username}")
+        else:
+            logger.info(f"Бот анонимно отправил картинку от @{call.from_user.username}")
+        user_message[call.from_user.id]["handler_trigger"] = 0
         user_message[call.from_user.id]["last_time"] = time()
         bot.edit_message_text(
             chat_id=call.message.chat.id, message_id=call.message.message_id, text="Фото успешно отправлено!"
         )
 
 
+logger.info("TIME TO WORK")
 bot.infinity_polling()
